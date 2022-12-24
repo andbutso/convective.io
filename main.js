a5d7054f2ba8ebfcb5f7ea98670adaad7237dcc9
andbutso <george@Georges-MacBook-Air.local>
2022-12-24 10:57:38 -0700

    new files

---
+++ main.js
@@ -0,0 +1,9 @@
+const html = document.documentElement;
+const canvas = document.querySelector('.globe-scrolling');
+const context = canvas.getContext('2d');
+
+const currentFrame = index => (
+	)
+
+
+
